c8f9680d1663c61af3028a5252dd1cca5226e217
Samson Emeje <semeje@secureidltd.com>
2022-06-19 14:25:15 +0100

    add serverless project

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'dycsls4sx6';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-djqj5gsu.us.auth0.com',
+  clientId: 'pORsZFCtNTgNx7XpPzEum5GE4Lgh6ONf',
+  callbackUrl: 'http://localhost:3000/callback'
+};
